89629f3878ccb8c653fece267b0208d98b5253c1
Sergio Francisco Anguiano Reyes <soli_frank_13@hotmail.com>
2016-03-22 01:23:44 -0600

    Cuarta Versión :D Se agrego propuesta de login y de Menú

--- OficioSeguro/Conexion/Datos.php
+++ OficioSeguro/Conexion/Datos.php
@@ -1,8 +1,19 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function conectarse()
+{
+ $conectarse =mysqli_connect("localhost","root","")  or die(mysql_error());
+ 
+ if(Conectarse)
+    {
+	
+	mysql_selectdb("isii",$conectarse);
+	return($conectarse);
 
+    }
+ else
+    {
+	return("-1");
+    }
+}
+?>
