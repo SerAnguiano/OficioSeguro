0e741305fd93ae4a088a0cf646b2fd6eb461fe17
Sergio Francisco Anguiano Reyes <soli_frank_13@hotmail.com>
2016-03-23 01:17:09 -0600

    Quinta Versión  :D Se agrego Carpeta Master Donde se pondra eñ codigo que no se quiere repetir en cuestión de diseño

---
+++ OficioSeguro/Vista/index.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>Oficio Seguro</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../css/csslogin.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <script src="http://mymaplist.com/js/vendor/TweenLite.min.js"></script>
+
+        <script src="../js/jslogin.js" type="text/javascript"></script>
+        
+    </head>
+   
+    <body>
+<div class="container">
+    <div class="row vertical-offset-100">
+    	<div class="col-md-4 col-md-offset-4">
+    		<div class="panel panel-default">
+			  	<div class="panel-heading">
+			    	<h3 class="panel-title">MORLOM</h3>
+			 	</div>
+                     <div class="panel-heading">                                
+                                <div align='center' class="row-fluid user-row">
+                                    <img  src="../Images/logo.jpg"  class="img-responsive" alt="Conxole Admin"/>
+                                    
+                                </div>
+                            </div>
+			  	<div class="panel-body">
+			    	<form accept-charset="UTF-8" role="form">
+                    <fieldset>
+			    	  	<div class="form-group">
+			    		    <input class="form-control" placeholder="E-mail" name="email" type="text">
+			    		</div>
+			    		<div class="form-group">
+			    			<input class="form-control" placeholder="Password" name="password" type="password" value="">
+			    		</div>
+			    		<div class="checkbox">
+			    	    	
+			    	    </div>
+			    		<input class="btn btn-lg btn-primary btn-block" type="submit" value="Login">
+			    	</fieldset>
+			      	</form>
+                                    
+			    </div>
+			</div>
+		</div>
+	</div>
+</div>
+    </body>
+</html>
---
+++ OficioSeguro/Vista/inicio.php
@@ -0,0 +1,38 @@
+
+<html>
+    <head>
+        <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/cssmenu.css" rel="stylesheet" type="text/css"/>
+        
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="../js/jsmenu.js" type="text/javascript"></script>
+          
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+  
+    <body>
+        
+        <?php
+            include("../MasterPage/MenuMaster.html");
+        ?>
+        <div id="page-content-wrapper">
+            <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+                <span class="hamb-top"></span>
+    			<span class="hamb-middle"></span>
+				<span class="hamb-bottom"></span>
+            </button>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <h1>Oficio Seguro</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </body>
+            
+</html>
