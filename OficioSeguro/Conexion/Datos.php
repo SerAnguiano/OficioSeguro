41f0fbcbbdaecfc5c725e54b9187351fa639c3cd
MIGUEL <MIGUEL@DESKTOP-M9TI9PN>
2016-03-22 21:16:05 -0600

    Actualización en cadena de conección

--- OficioSeguro/Conexion/Datos.php
+++ OficioSeguro/Conexion/Datos.php
@@ -3,14 +3,14 @@
 $conexion =  mysql_connect("localhost","root");
 if(!$conexion)
 {
-    die("no se ha podido conectar al servidor MYSQL: ".mysql_error());	
+    die("No se ha podido conectar al servidor MYSQL: ".mysql_error());	
 }
 
 $bd_seleccionada = mysql_select_db("Proyecto",$conexion);
 
 if(!$bd_seleccionada)
 {
-    die("no se ha podido seleccionar la base de datos: ".mysql_error());	
+    die("No se ha podido seleccionar la base de datos: ".mysql_error());	
 }
 
 ?>
